838856057f23b87805692cbf70ccd59d06b62a0f
aebrahim <aebrahim@THQ1LTAEBRAHIM>
2019-07-05 00:48:34 +0300

    100 % of test cases passed

--- HackerRank_Insert_Node_at_Tail_of_Linked_List/Program.cs
+++ HackerRank_Insert_Node_at_Tail_of_Linked_List/Program.cs
@@ -1,15 +1,115 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace HackerRank_Insert_Node_at_Tail_of_Linked_List
+class Solution
 {
-    class Program
+
+    class SinglyLinkedListNode
     {
-        static void Main(string[] args)
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
         {
+            this.data = nodeData;
+            this.next = null;
         }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+        }
+
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
+    {
+        while (node != null)
+        {
+            Console.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                Console.Write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+    {
+        SinglyLinkedListNode NodeAtTail = new SinglyLinkedListNode(data);
+
+        if (head == null)
+        {
+            head = NodeAtTail;
+        }
+        else
+        {
+
+            if (head.next != null)
+            {
+                insertNodeAtTail(head.next, data);
+            }
+            else
+            {
+                head.next = NodeAtTail;
+            }
+        }
+        return head;
+
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++)
+        {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+            llist.head = llist_head;
+
+        }
+
+
+
+        PrintSinglyLinkedList(llist.head, "\n");
+        //textWriter.WriteLine();
+
+        //textWriter.Flush();
+        //textWriter.Close();
     }
 }
